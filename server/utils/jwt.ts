3e0301c1aafead55513ab25592086d2a967d8076
Boyang Niu <boyang.niu@gmail.com>
2022-03-09 15:35:40 -0800

    Cookie

--- server/utils/jwt.ts
+++ server/utils/jwt.ts
@@ -7,6 +7,14 @@ export function setJwtHeaderOnLogin(ctx: RouterContext, user: User) {
   const newToken = jwt.sign({ userId: user.id }, config.auth.JWT_SECRET, {
     expiresIn: "30d",
   });
+  ctx.cookies.set("auth_token", newToken, {
+    maxAge: 86400000 * 30,
+    overwrite: true,
+    httpOnly: true,
+    secure: config.app.IS_PROD,
+    sameSite: config.app.IS_PROD ? "none" : "lax",
+    path: "/",
+  });
   ctx.body = {
     access_token: newToken,
     token_type: "Bearer",
