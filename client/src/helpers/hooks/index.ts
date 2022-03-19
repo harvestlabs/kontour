37b423bffaec6071fdc9d16d34c43dd2fe5b472e
dxu <xudavidb@gmail.com>
2022-03-18 19:08:27 -0700

    add authenticatedsubscription

--- client/src/helpers/hooks/index.ts
+++ client/src/helpers/hooks/index.ts
@@ -1,4 +1,14 @@
-import { useLayoutEffect, useState } from "react";
+import {
+  DocumentNode,
+  OperationVariables,
+  SubscriptionHookOptions,
+  SubscriptionResult,
+  TypedDocumentNode,
+  useSubscription,
+} from "@apollo/client";
+import { createGraphQLClient } from "@gql/GraphQLClient";
+import { AuthContext } from "@utils/auth";
+import { useContext, useLayoutEffect, useMemo, useState } from "react";
 
 export function useWindowResize(onWindowResize: () => void, deps?: any[]) {
   const [size, setSize] = useState([0, 0]);
@@ -12,4 +22,20 @@ export function useWindowResize(onWindowResize: () => void, deps?: any[]) {
     return () => window.removeEventListener("resize", updateSize);
   }, [onWindowResize, ...(deps || [])]);
   return size;
+}
+
+export function useAuthenticatedSubscription<
+  TData = any,
+  TVariables = OperationVariables
+>(
+  subscription: DocumentNode | TypedDocumentNode<any, OperationVariables>,
+  options?: SubscriptionHookOptions<TData, TVariables>
+): SubscriptionResult<TData, any> {
+  const authToken = useContext(AuthContext);
+  const client = useMemo(() => createGraphQLClient(authToken), [authToken]);
+  const optionsWithAuth = {
+    ...options,
+    client,
+  };
+  return useSubscription<TData, TVariables>(subscription, optionsWithAuth);
 }
