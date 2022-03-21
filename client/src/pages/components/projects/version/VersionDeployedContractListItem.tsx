c50b326b2a7de5979caca7c7488edd3de3a5bf46
Boyang Niu <boyang.niu@gmail.com>
2022-03-21 00:45:42 -0700

    Hover

--- client/src/pages/components/projects/version/VersionDeployedContractListItem.tsx
+++ client/src/pages/components/projects/version/VersionDeployedContractListItem.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { ListIcon, ListItem } from "@chakra-ui/react";
 import { gql, useMutation } from "@apollo/client";
 import { VersionDeployedContractListItemFragment } from "@gql/__generated__/VersionDeployedContractListItemFragment";
@@ -12,10 +12,17 @@ export default function VersionDeployedContractListItem({
   icon,
   contract,
 }: Props) {
+  const [hover, setHover] = useState(false);
   return (
-    <ListItem display="flex" alignItems="center">
+    <ListItem
+      display="flex"
+      alignItems="center"
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+    >
       {icon ? <ListIcon as={icon} width="12px" /> : null}
-      {contract?.contractSource?.name}: {contract.address.substring(0, 8)}...
+      {contract?.contractSource?.name}:{" "}
+      {hover ? contract.address : contract.address.substring(0, 8)}...
     </ListItem>
   );
 }
